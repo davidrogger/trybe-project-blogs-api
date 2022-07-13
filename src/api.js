4dd9b7cecb2907fbc636d006dbe3e044f1a3eaf6
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-13 17:06:15 -0300

    feat: login route
    created route to login

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
-// initial pr
+require('express-async-errors');
+const errorHandler = require('./middlewares/errorHandler');
+const loginRoutes = require('./routes/loginRoutes');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRoutes);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
+
+app.use(errorHandler);
+
 module.exports = app;
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+const statusLibrary = {
+  ValidationError: 400,
+  NotFound: 400,
+};
+
+module.exports = ({ name, message }, _req, res, _next) => {
+  const status = statusLibrary[name] || 500;
+
+  res.status(status).json(message);
+};
