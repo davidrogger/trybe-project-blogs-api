f013cafc6aa0526824d647eab40d8dc0a315890a
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-13 18:53:12 -0300

    feat: change validation
    change to use a generic message just to validate login and create validation to newUser

--- src/services/validationService.js
+++ src/services/validationService.js
@@ -2,10 +2,7 @@ const Joi = require('joi');
 
 const runSchema = (schema) => async (itemValidate) => {
   const { value, error } = await schema.validate(itemValidate);
-  if (error) {
-    error.message = 'Some required fields are missing';
-    throw error;
-  }
+  if (error) throw error;
   return value;
 };
 
@@ -13,5 +10,14 @@ module.exports = {
   loginBody: runSchema(Joi.object({
     email: Joi.string().required(),
     password: Joi.string().required(),
+  }).messages({
+    'any.required': 'Some required fields are missing',
+    'string.empty': 'Some required fields are missing',
+  })),
+  newUserBody: runSchema(Joi.object({
+    displayName: Joi.string().min(8).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    image: Joi.string(),
   })),
 };
