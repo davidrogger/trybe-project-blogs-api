651e5e26e4168287102a4d857a56a60a182d7a15
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-14 10:51:46 -0300

    feat: blogpost model
    created sequelize model to blogpost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,22 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'users' }
+      )
+  }
+
+  return BlogPost;
+};
