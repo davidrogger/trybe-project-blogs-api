817e2b7560723411a8967800a384ac5da830a8dd
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-14 09:52:50 -0300

    feat: change validate service name
    change the name to be more clear when reading

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,10 +1,10 @@
 const userService = require('../services/userService');
-const validationService = require('../services/validationService');
+const validate = require('../services/validationService');
 const jwtService = require('../utils/tokenJWT');
 
 module.exports = {
   async auth(req, res) {
-    const user = await validationService.loginBody(req.body);
+    const user = await validate.loginBody(req.body);
     const userData = await userService.login(user);
     const token = jwtService.tokenGenerator(userData);
     res.status(200).json({ token });
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,11 @@
 const userService = require('../services/userService');
-const validationService = require('../services/validationService');
+const validate = require('../services/validationService');
 const jwtService = require('../utils/tokenJWT');
 const { ErrorCustom } = require('../utils/errosCustom');
 
 module.exports = {
   async create(req, res) {
-    const newUser = await validationService.newUserBody(req.body);
+    const newUser = await validate.newUserBody(req.body);
     const user = await userService.exists(newUser);
     if (user > 0) throw new ErrorCustom('User already registered', 'AlreadyRegistered');
     const userData = await userService.create(newUser);
@@ -17,7 +17,7 @@ module.exports = {
     res.status(200).json(allUsers);
   },
   async getById(req, res) {
-    const { id } = await validationService.userId(req.params);
+    const { id } = await validate.userId(req.params);
     const user = await userService.getById(id);
     res.status(200).json(user);
   },
