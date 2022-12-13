54abeb0b4e14f2cb658edd3082452a10eb9f1e1d
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-13 13:34:49 -0300

    feat: added route to create a new user with examples

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,9 @@ const authentication = require('../middlewares/authentication');
 const userRoute = Router();
 
 userRoute.post('/', userController.create);
+
 userRoute.use(authentication.auth);
+
 userRoute.get('/:id', userController.getById);
 userRoute.delete('/me', userController.remove);
 userRoute.get('/', userController.getAll);
